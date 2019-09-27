aac5e8ebeed2694e0420d79220bd740e97798757
Student <Student@201-1>
2019-09-27 19:34:59 +0300

    why

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 const int ROWS = 4;	//Количество строк двумерного массива
 const int COLS = 5;	//Количество элементов строки
 
-void FillRand(int Arr[], const int n);
+template<typename T> 
+void FillRand(T Arr[], const int n);
 void FillRand(double Arr[], const int n);
 void FillRand(int Arr[ROWS][COLS], const int ROWS, const int COLS);
 
@@ -40,7 +41,8 @@ void main()
 	Print(Crr, ROWS, COLS);
 }
 
-void FillRand(int Arr[], const int n)
+template<typename T>
+void FillRand(T Arr[], const int n)
 {
 	//Заполнение случайными числами:
 	for (int i = 0; i < n; i++)
